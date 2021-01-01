41eb363077030a6b0df5a641394bf45f58d983a7
pete <pete@pop-os.localdomain>
2021-01-01 21:26:59 +0000

    Adding ffmpeg.sh to the project

---
+++ ffmpeg.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#ffmpeg -i input.mp3 -metadata artist="Someone" -f mp3 output.mp3
+
+function filename_remove_percent() {
+
+# Convert percentages found in filename to ascii equivalent
+# eg:  %20 needs to be converted into a space character
+
+for file_old in *.mp3 ; do
+	file_new=$(echo ${file_old} | sed 's/%20/ /g' )
+
+	mv -v "${file_old}" "${file_new}"
+done
+}
+
+function metadata_add() {
+
+	metadata='-metadata artist="Usher" '
+        metadata+='-metadata account_id= '
+}
+
+metadata_add
+
+for file in *.mp3 ; do
+	ffmpeg -loglevel panic -i "${file}" ${metadata} -f mp3 "output_${file}" &
+done
