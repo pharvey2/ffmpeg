00d176abe905c4ac00814ed1bd277a0a2a641ea8
pete <pete@pop-os.localdomain>
2021-01-01 22:15:45 +0000

    ffmpeg comments

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -3,10 +3,10 @@
 #ffmpeg -i input.mp3 -metadata artist="Someone" -f mp3 output.mp3
 
 function filename_remove_percent() {
-
+#
 # Convert percentages found in filename to ascii equivalent
 # eg:  %20 needs to be converted into a space character
-
+#
 for file_old in *.mp3 ; do
 	file_new=$(echo ${file_old} | sed 's/%20/ /g' )
 
@@ -15,13 +15,18 @@ done
 }
 
 function metadata_add() {
-
+#
+# List all metadata tags that are to be globally updated
+#
 	metadata='-metadata artist="Usher" '
         metadata+='-metadata account_id= '
 }
 
-metadata_add
+metadata_add ; # Setup mp3 metadata tags
 
+#
+# Multi-thread the ffmpeg processing of each MP3 track.
+#
 for file in *.mp3 ; do
 	ffmpeg -loglevel panic -i "${file}" ${metadata} -f mp3 "output_${file}" &
 done
